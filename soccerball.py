0b42d509935cd5823cafe0d02751bf7579a0c0ce
macksjeremy <31860185+macksjeremy@users.noreply.github.com>
2020-11-25 03:29:26 -0500

    Add files via upload

--- soccerball.py
+++ soccerball.py
@@ -1,6 +1,24 @@
 import nltk
+from nltk.corpus import words as words
+import time
+
+
+'''
+def isvowel(letter):
+    if letter in ['a','e','i','o','u']:
+        return True
+    else:
+        return False
+#def threeconsonants(stringinput):
+
+def trippling(chars):
+    return chars + chars + chars
+
+def threeletter(chars, string):
+    map()
 
 def works(input):
+    print(english_vocab)
     if(len(input) < 7):
         return False
     letters = ['y', 'b', 'i', 'a', 'l', 'u', 'n']
@@ -8,7 +26,6 @@ def works(input):
         if i in letters:
             letters.remove(i)
     if (len(letters)==0):
-        english_vocab = set(w.lower() for w in nltk.corpus.words.words())
         if input in english_vocab:
             print(input)
             return True
@@ -17,15 +34,52 @@ def works(input):
 
 def build(stringput):
     letters = ['b', 'i', 'a', 'l', 'u', 'n','y']
-    print(stringput)
+    if (len(stringput) == 10 or "yy" in stringput or "aa"in stringput or "ii" in stringput or "uu" in stringput):
+        return
+    #print(stringput)
     if(works(stringput)):
-        print(stringput)
-    if (len(stringput) == 8 or "yy" in stringput or "aa"in stringput or "ii" in stringput or "uu" in stringput):
-            return
+         print(stringput)
     for i in letters:
         build(stringput + i)
+#
+#
+#Maybe i'm coming at this the wrong way.
+#build("")
+'''
+#No longer necessary, but useful!
+#def split(word):
+#    return [char for char in word]
+def removelist(input,string):
+    #print(string)
+    strcpy = string
+    for i in input:
+        strcpy = strcpy.replace(i,"")
+    #print(strcpy)
+    if len(strcpy) > 0:
+        return False
+    else:
+        print(string)
+        return True
+
+def parsethrough(input):
+    english_vocab = [w.lower() for w in words.words()]
+    for i in english_vocab:
+        found = True
+        for j in input:
+            if j not in i:
+                found = False
+        if found and removelist(input, i):
+            #check other letter possiblity
+            print(i)
+            return True
+
+#So it turns out wordnet does not contain every word in the english language.
 
 
 
-build("")
+k = time.time()
+input = ['b', 'i', 'a', 'l', 'u', 'n', 'y']
+#print(words.words())
+parsethrough(input)
+print("Time elapsed is:",time.time() - k)
 
